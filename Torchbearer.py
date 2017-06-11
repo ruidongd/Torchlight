e25d97dcda63e77ff993a1a6198204aa218f742a
ruidongd <ruidongd@outlook.com>
2017-06-12 05:43:28 +0800

    nothing

--- Torchbearer.py
+++ Torchbearer.py
@@ -11,19 +11,19 @@ import itertools # This is rather important for combinatorial work.
 from timeit import default_timer as timer
 
 <<<<<<< HEAD
-trial6x6 =  ''' 
+trial6x6 =  '''
                 <DrawingDecorator>
                     <DrawCuboid x1="0" y1="39" z1="0" x2="5" y2="39" z2="5" type="obsidian"/>
                 </DrawingDecorator>
             '''
-            
-trial8x8 =  ''' 
+
+trial8x8 =  '''
                 <DrawingDecorator>
                     <DrawCuboid x1="0" y1="39" z1="0" x2="7" y2="39" z2="7" type="obsidian"/>
                 </DrawingDecorator>
             '''
-            
-trial10x10 =    ''' 
+
+trial10x10 =    '''
                 <DrawingDecorator>
                     <DrawCuboid x1="0" y1="39" z1="0" x2="9" y2="39" z2="9" type="obsidian"/>
                 </DrawingDecorator>
@@ -31,8 +31,8 @@ trial10x10 =    '''
 
 def GetMissionXML( trial ):
     # generatorString = 2;0;127; for MC v1.7 and below
-    
-    return 
+
+    return
     '''
         <?xml version="1.0" encoding="UTF-8" ?>
         <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
@@ -54,7 +54,7 @@ def GetMissionXML( trial ):
                       <ServerQuitWhenAnyAgentFinishes/>
                     </ServerHandlers>
                  </ServerSection>
-                  
+
                  <AgentSection mode="Creative">
                     <Name>Torchbearer</Name>
                     <AgentStart>
@@ -76,12 +76,12 @@ def GetMissionXML( trial ):
                     </AgentHandlers>
                   </AgentSection>
                 </Mission>'''
-                
+
 def placeTorch():
     agent_host.sendCommand("use 1")
     time.sleep(0.1)
     agent_host.sendCommand("use 0")
-    
+
 def findCenter(x, z):
     retX = math.floor(x/2)
     retZ = math.floor(z/2)
@@ -122,15 +122,15 @@ def teleport(self, agent_host, teleport_x, teleport_z):
 
 def update_list(self, l, row, col, num):
     l[row][col] = 14
-    
+
     for r in range(num):
         for c in range(num):
             temp = max(14-abs(row-r)-abs(col-c),0)
             l[r][c] =  temp if l[r][c] < temp else l[r][c]
-    
+
 
 =======
-trial6x6 = 	'''	
+trial6x6 = 	'''
 	<DrawingDecorator>
 		<DrawCuboid x1="0" y1="40" z1="0" x2="15" y2="40" z2="15" type="air"/>
 		<DrawCuboid x1="0" y1="39" z1="0" x2="5" y2="39" z2="5" type="obsidian"/>
@@ -138,7 +138,7 @@ trial6x6 = 	'''
 	</DrawingDecorator>
 	'''
 
-trial8x8 = 	'''	
+trial8x8 = 	'''
 	<DrawingDecorator>
 		<DrawCuboid x1="0" y1="40" z1="0" x2="15" y2="40" z2="15" type="air"/>
 		<DrawCuboid x1="0" y1="39" z1="0" x2="7" y2="39" z2="7" type="obsidian"/>
@@ -146,14 +146,14 @@ trial8x8 = 	'''
 	</DrawingDecorator>
 	'''
 
-trial10x10 = 	'''	
+trial10x10 = 	'''
 	<DrawingDecorator>
 		<DrawCuboid x1="0" y1="40" z1="0" x2="15" y2="40" z2="15" type="air"/>
 		<DrawCuboid x1="0" y1="39" z1="0" x2="9" y2="39" z2="9" type="obsidian"/>
 		<DrawCuboid x1="0" y1="39" z1="0" x2="15" y2="39" z2="15" type="air"/>
 	</DrawingDecorator>
 	'''
-	
+
 def CreateTrial( n , startingCoordinates):
 	# Creates an nXn square. Erases ground level and one above; rewrites with obsidian.
 	"""
@@ -161,7 +161,7 @@ def CreateTrial( n , startingCoordinates):
 				n:						<int>	How big the square is.
 				startingCoordinates:	<tuple(int, int)>	Where the square begins. This is important if we are to make multi-grid tests.
 	"""
-	
+
 	return '''
 	<DrawingDecorator>
 		<DrawCuboid x1="''' + str(startingCoordinates[0]) + '''" y1="40" z1="''' + str(startingCoordinates[1]) + '''" x2="''' + str(n - 1) + '''" y2="40" z2="''' + str(n - 1) + '''" type="air"/>
@@ -217,54 +217,54 @@ class Torchbearer(object):
 	def __init__(self, trialsize, doable):
 		"""
 		Create Torchbearer AI, with empty lists of coordinates.
-		
+
 		Args:
 			trialsize:	<int>	The size of the square that the AI will iterate over, as an nXn square.
 			doable:		<int>	The minimum amount of torches the trial can be done in.
 		"""
-		
+
 		# NOTE: A majority of this code is deprecated, but remains as a back up if our new code fails.
 		# See "V 1.0" at the bottom.
-		
+
 		self.currentList = [] # What our current light levels are at.
 		self.startList = [] # What coordinates we start with.
-		
+
 		for i in range(trialsize):
 			self.currentList.append([0]*trialsize)
 			for j in range(trialsize):
 				self.startList.append((i,j))
-		
+
 		self.startList = self.doableList(doable, self.startList) # Create a list of coordinates to do. DOES NOT WORK WITH V 1.0.
-		
+
 		self.triedList = []
 		self.bestList = []
-		
+
 		self.position = (0,0)
-		
+
 		self.trial = trialsize
 		self.currentTorches = []
 		self.worst = []
-		
-		# Create a dict of scores -- 
+
+		# Create a dict of scores --
 		self.scoredList = dict()
-		
+
 		#currentList = list of light levels of the CURRENT mission
-		#triedList = list of coordinates we have TRIED already, as a list of combinations/final (x,z) coordinates 
+		#triedList = list of coordinates we have TRIED already, as a list of combinations/final (x,z) coordinates
 			#(or: [ [(tuple), (tuple), (tuple)], [(tuple), (tuple), (tuple)] ])
-		#bestList = list of BEST coordinates to place torches, as a list of (x,z) coordinates 
-			#(or: [(tuple), (tuple), (tuple)]) 
+		#bestList = list of BEST coordinates to place torches, as a list of (x,z) coordinates
+			#(or: [(tuple), (tuple), (tuple)])
 			#(chosen compared to bList, based on number of torches placed/len of the combination)
-			
+
 		#currentTorches = list of tuples in the current run.
 		#worst = the longest list of tuples in triedList so far.
-		
+
 		# scoredList: a dictionary of "scores" and their coordinates. The score is calculated based on how many squares are lit up.
-		
+
 	def doableList(self, doable, startingList):
 		# Creates a list of all combinations in len(startingList) C doable (as an nCr function)
-		
+
 		# TODO: Start at the center and move outwards.
-		
+
 		retList = []
 		for i in itertools.combinations(startingList,doable):
 			if(sorted(list(i)) not in retList):
@@ -291,7 +291,7 @@ class Torchbearer(object):
 		time.sleep(0.1)
 		agent_host.sendCommand("use 0")
 		self.updateLists()
-		
+
 
 	def findCenter(self, x, z):
 		# Finds center of trial
@@ -318,7 +318,7 @@ class Torchbearer(object):
 					good_frame = True
 					end_frame = timer()
 		self.position = (teleport_x, teleport_z)
-		
+
 	def clearSelf(self):
 		self.currentList = []
 		for i in range(self.trial):
@@ -387,8 +387,8 @@ for iRepeat in range(num_reps):
         #   msg = world_state.observations[-1].text                     # Yes, so get the text
         #   observations = json.loads(msg)                              # and parse the JSON
         #   grid = observations.get(u'floor11x11', 0)                   # and get the grid we asked for
-            
-            
+
+
 =======
 # Create default Malmo objects:
 if __name__ == '__main__':
@@ -417,26 +417,26 @@ if __name__ == '__main__':
 	trial = CreateTrial(num, (0,0))
 	#
 	#
-	
+
 	# Doable: the minimum amount of torches needed to fill up a num by num space.
 	# Because torches light up 14 on their own square and taxi cab downwards...
 	# ... if we start in the center of a 7x7 matrix:
-	
-	#	8	9	10	11			
-	#	9	10	11	12			
-	#	10	11	12	13			
+
+	#	8	9	10	11
+	#	9	10	11	12
+	#	10	11	12	13
 	#	11	12	13	14	13	12	11
-	#				13	12		
-	#				12		10	
+	#				13	12
+	#				12		10
 	#				11			8
-	
+
 	# The same goes for a 6x6 matrix onwards.
 	# If you make an 8x8 or 9x9 matrix, you require 2 torches.
 	# If you make a 10x10 or 11x11 matrix, you require 3.
 	# And so on.
-	
+
 	# Essentially: take your nXn matrix, divide by 2, round down, subtract 2, and that's how much your doable is.
-	
+
 	doable = 0
 	if(trial == trial6x6):
 		num = 6
@@ -444,7 +444,7 @@ if __name__ == '__main__':
 		num = 8
 	elif(trial == trial10x10):
 		num = 10
-		
+
 	doable = int(math.floor(num/2)-2)
 	num_reps = min(num**2, 500) + 1
 	# num_reps = 10
@@ -462,7 +462,7 @@ if __name__ == '__main__':
 		my_mission.allowAllAbsoluteMovementCommands()
 		my_mission.requestVideo(800, 500)
 		my_mission.setViewpoint(0)
-		
+
 
 		# Attempt to start a mission:
 		max_retries = 3
@@ -476,7 +476,7 @@ if __name__ == '__main__':
 					exit(1)
 				else:
 					time.sleep(2)
-			
+
 		# Loop until mission starts:
 		print "Waiting for the mission to start ",
 		world_state = agent_host.getWorldState()
@@ -489,25 +489,25 @@ if __name__ == '__main__':
 
 		print
 		print "Mission running # ",iRepeat+1
-		
+
 		world_state = agent_host.getWorldState()
 		for error in world_state.errors:
 			print "Error:",error.text
-		
+
 		center = torchbearer.findCenter(num, num)
 		# print("Are we running?",world_state.is_mission_running)
 
 		agent_host.sendCommand("pitch 1")
 		# print("Trying to look down")
 		time.sleep(0.5)
-		
+
 		# Control center torch
 		# torchbearer.teleport(agent_host, center[0], center[1])
 		# torchbearer.placeTorch(agent_host)
 
 		# If this is not the "victory lap":
 		if(iRepeat != num_reps - 1):
-		
+
 			# Check Torchbearer for list of light levels; quit if all are 8 or lower.
 			if(len(torchbearer.startList) > 0):
 				tryThis = torchbearer.startList[0] # tryThis is a list of 2-tuples/coordinates.
@@ -530,11 +530,11 @@ if __name__ == '__main__':
 				# print(torchbearer.scoredList)
 				torchbearer.startList.remove(torchbearer.startList[0])
 				torchbearer.clearSelf()
-				
+
 			#															#
 			#			V 1.0, without doablelist implemented!			#
 			#															#
-			
+
 			'''
 			# Start at this location first; technically iterating over the whole list, slowly.
 			if(len(torchbearer.startList) > 0):
@@ -542,12 +542,14 @@ if __name__ == '__main__':
 				torchbearer.teleport(agent_host, coordinate[0], coordinate[1])
 				torchbearer.placeTorch(agent_host)
 				torchbearer.startList.remove(coordinate)
-			
-			
 
-			
+
+
+
 			# The current run's torchbearing.
 			while True:
+                dark = True if any(pos < 8 for pos in l for l in torchbearer.currentList) else False
+                # one line version for following code
 				dark = False;
 				for i in torchbearer.currentList:
 					for j in i:
@@ -601,11 +603,11 @@ if __name__ == '__main__':
 							if(b == lowest):
 								darkList.append((a,i))
 				# print(darkList)
-				
+
 				# COMPLETELY RANDOM DISTRIBUTION
 				# NO SMART CHECKS
 				# NEED ALGORITHM
-				
+
 				# Check list of dark (light < 8) squares; check if this string of coordinates has already turned out a solution.
 				# If it has, do not continue.
 				tempDarkList = []
@@ -616,18 +618,18 @@ if __name__ == '__main__':
 					tryTheseTorches.append(i)
 					if(sorted(tryTheseTorches) in torchbearer.triedList):
 						darkList.remove(i)
-						
+
 				if(len(darkList) == 0):
 					print("Already tried this solution.")
 					torchbearer.clearSelf()
 					break
-				
+
 				rando = random.randint(0, len(darkList) - 1)
-				
+
 				torchbearer.teleport(agent_host, darkList[rando][0], darkList[rando][1])
 				torchbearer.placeTorch(agent_host)
 				torchbearer.currentTorches.append(torchbearer.position)
-				
+
 				# Check if the placed torches are either above our suspected control break, or more torches than our worst solution at the very start.
 				# If we end up getting lucky with a 1 torch solution, then we never have to check any higher.
 				# Because of how we iterate initially (off startList), that will rarely happen...
@@ -636,23 +638,23 @@ if __name__ == '__main__':
 					print("Inefficient choices.")
 					torchbearer.clearSelf()
 					break
-				
+
 			#DEBUG BREAK
 			#break
-			# print(torchbearer.triedList) 
+			# print(torchbearer.triedList)
 			'''
-			
+
 			#															#
 			#						END V 1.0							#
 			#															#
-			
-			
+
+
 			agent_host.sendCommand("quit")
 			time.sleep(0.1)
-		
+
 		else:
 			lowest = None
-				
+
 			for i in torchbearer.scoredList: # First iterate to find the lowest score in torchbearer.scoredList and if it contains coordinates.
 				if((lowest == None or i < lowest) and len(torchbearer.scoredList[i]) > 0):
 					lowest = i
@@ -661,20 +663,20 @@ if __name__ == '__main__':
 			print("Best Coordinates for grid {}x{}:".format(num,num))
 			for i in torchbearer.bestList:
 				print(i)
-				
+
 			print("Scores:")
 			for i in torchbearer.scoredList:
 				print("{} dark squares: {}".format(i,torchbearer.scoredList[i]))
-				
+
 			rando = random.randint(0, len(torchbearer.bestList) - 1)
 			print("Showing possible solution: {}".format(torchbearer.bestList[rando]))
 			for i in torchbearer.bestList[rando]:
 				torchbearer.teleport(agent_host,i[0],i[1])
 				torchbearer.placeTorch(agent_host)
-			
+
 			agent_host.sendCommand("quit")
 			time.sleep(0.1)
-			
+
 		'''
 	# Iterate twice: once to check for the lowest length amongst solutions, the other to add them.
 	# print(torchbearer.worst)
@@ -692,10 +694,10 @@ if __name__ == '__main__':
 		# print(len(i))
 		if(len(i) == bestLength):
 			torchbearer.bestList.append(i)
-			
+
 	print("Best locations: ")
 	for i in torchbearer.bestList:
 		print(i)
 		'''
-		
+
 >>>>>>> Raustana/master
